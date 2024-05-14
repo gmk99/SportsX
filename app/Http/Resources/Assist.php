90ae9076beb708be89b456e45bdcb8224bddf998
TiagoPachecoRibeiro <tiago.p.ribeiro04@gmail.com>
2024-05-14 14:16:22 +0100

    Assist Resources

--- app/Http/Resources/Assist.php
+++ app/Http/Resources/Assist.php
@@ -8,17 +8,11 @@ class Assist extends JsonResource
     public function toArray($request)
     {
         return [
-            'assistID' => $this->assistID,
-            'minute' => $this->minute,
-            'assistType' => $this->assistType,
-            'player' => [
-                'playerID' => $this->player->playerID,
-                'fullName' => $this->player->fullName,
-                'birthdate' => $this->player->birthdate,
-                'associationNumber' => $this->player->associationNumber,
-                'position' => $this->player->position,
-                'number' => $this->player->number,
-            ],
+            'AssistID' => $this->AssistID,
+            'Minute' => $this->Minute,
+            'GameID' => $this->GameID,
+            'PlayerID' => $this->PlayerID,
+            'GoalID' => $this->GoalID,
         ];
     }
 }
