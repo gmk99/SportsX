b1d38d2545c71db133098c65d18de9c6e1a021d7
TiagoPachecoRibeiro <tiago.p.ribeiro04@gmail.com>
2024-05-14 14:10:57 +0100

    Updated App Folder

--- app/Http/Resources/Player.php
+++ app/Http/Resources/Player.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Resources;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\Player as PlayerModel;
 
 class Player extends JsonResource
 {
+    public static function all()
+    {
+        return PlayerModel::all();
+    }
+
     public function toArray($request)
     {
         return [
