3ab1ede79537b3d4917d337d6664738cf464fc54
TiagoPachecoRibeiro <tiago.p.ribeiro04@gmail.com>
2024-05-22 14:18:29 +0100

    Projeto

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -62,7 +62,7 @@
                         class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="ni ni-credit-card text-success text-sm opacity-10"></i>
                     </div>
-                    <span class="nav-link-text ms-1">Billing</span>
+                    <span class="nav-link-text ms-1">Gestão de Lesões</span>
                 </a>
             </li>
             <li class="nav-item">
